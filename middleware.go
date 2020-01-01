ffb56a22ef7c901c6f24c95f8816b00e9ac1ac2f
saeed <saeid.kamyabi71@gmail.com>
2020-01-01 17:01:12 +0330

    optional auth on any request beside refreshing token

--- middleware.go
+++ middleware.go
@@ -6,17 +6,22 @@ import (
 	"github.com/go-m/auth/refresh"
 )
 
-func middleware(request gm.IRequest) (result interface{}) {
+func (h *Handler) middleware(request gm.IRequest) (result interface{}) {
 	req := request.GetBaseRequest()
 	ctx := req.GetContext()
 	refreshToken := ctx.GetHeader(base.CurrentConfig.RefreshTokenHeaderKey)
-	if refreshToken == "" {
-		return
+	if refreshToken != "" {
+		token, err := refresh.GetAndDeleteToken(refreshToken)
+		if err != nil {
+			return
+		}
+		if token != nil {
+			err = base.HandleLoginResponse(request, token.Key, token.KeyType, true)
+			return
+		}
 	}
-	token, err := refresh.GetAndDeleteToken(refreshToken)
-	if err != nil || token == nil {
-		return
+	if h.config.OptionalAuthOnAnyRequest {
+		_ = h.config.LoginHandler.Authenticate(request)
 	}
-	err = base.HandleLoginResponse(request, token.Key, token.KeyType, true)
 	return
 }
